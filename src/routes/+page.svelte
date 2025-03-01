14afb43caf987c67e2d6b83bd314769b7bd001e5
Max <heemo@bk.ru>
2025-03-02 01:19:23 +0300

    100

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,29 @@
 <h1>Homepage🏢📈6️⃣</h1>
+
+<script>
+  import Gallery from 'svelte-image-gallery';
+</script>
+
+<Gallery gap="10" maxColumnWidth="200">
+  <img src="/arch/1.png" alt="">
+  <img src="/arch/2.png" alt="">
+  <img src="/arch/10.png" alt="">
+  <img src="/arch/3.png" alt="">
+  <img src="/arch/4.png" alt="">
+  <img src="/arch/5.png" alt="">
+  <img src="/arch/6.png" alt="">
+  <img src="/arch/7.png" alt="">
+  <img src="/arch/9.png" alt="">
+</Gallery>
+
+<style>
+  :global(img) {
+    opacity: 0.9;
+    transition: all 0.2s;
+  }
+
+  :global(img:hover) {
+    opacity: 1;
+    transform: scale(1.04);
+  }
+</style>
--- src/routes/advanched/+page.svelte
+++ src/routes/advanched/+page.svelte
@@ -1,1 +1,44 @@
 <p><a href="/emoji">emoji</a></p>
+
+<script>
+  const images = [
+    '/arch/1.png',
+    '/arch/2.png',
+    '/arch/3.png',
+    '/arch/4.png',
+    '/arch/5.png',
+    '/arch/6.png',
+    '/arch/7.png',
+    '/arch/8.png',
+    '/arch/9.png',
+    '/arch/10.png'
+  ];
+</script>
+
+<div class="gallery">
+  {#each images as image}
+    <img src={image} alt="">
+  {/each}
+</div>
+
+<style>
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 10px;
+    padding: 10px;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    opacity: 0.9;
+    transition: all 0.2s;
+    border-radius: 8px;
+  }
+
+  img:hover {
+    opacity: 1;
+    transform: scale(1.04);
+  }
+</style>
