e627562f865ee4a45649c0f8a85c7057a7c58245
Max <heemo@bk.ru>
2025-03-02 19:54:51 +0300

    4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
-<a href="/inter-tabl">interactive calendar</a>
-<h1>Header</h1>
-<h2>Header</h2>
+<a href="/inter-tabl"><h1>interactive calendar h1</h1></a>
+<a href="/palette"><h2>palette h2</h2></a>
 <h3>Header</h3>
 <h4>Header</h4>
 <h5>Header</h5>
-<h6>Header</h6>
+<h6>Header </h6>
 <a href='https://doka.guide/css/web-colors/'>Палитра цветов</a>
